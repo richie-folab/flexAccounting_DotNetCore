b0dd86cc2c127aa42a0983f7c9d11cdf0a9488b1
Richard Afolabi <richie_folab@yahoo.com>
2022-04-23 06:50:45 +0200

    first commit

---
+++ Context/FlexAccountingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlexAccounting.Context
+{
+    public class FlexAccountingDbContext : DbContext
+    {
+        public FlexAccountingDbContext(DbContextOptions<FlexAccountingDbContext> options):base(options)
+        {
+                
+        }
+    }
+}
---
+++ Models/ApplicationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace FlexAccounting.Models
+{
+    public class ApplicationRequest
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? CreatedBy { get; set; }
+    }
+}
